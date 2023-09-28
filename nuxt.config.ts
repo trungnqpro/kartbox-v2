9927ec581d53a49416e2d116b59719f25690135e
Nguyen Trung <nguyentrung@Nguyens-iMac.local>
2023-09-28 16:52:06 +0700

    Initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,110 @@
+import { createResolver } from '@nuxt/kit'
+const { resolve } = createResolver(import.meta.url)
+import { dirname } from 'node:path'
+import { fileURLToPath } from 'url'
+import VueI18nVitePlugin from '@intlify/unplugin-vue-i18n/vite'
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  // exp
+  experimental: {
+    localLayerAliases: true,
+  },
+
+  // app config
+  app: {
+    // global transition
+    pageTransition: { name: 'page', mode: 'out-in' },
+    layoutTransition: { name: 'layout', mode: 'out-in' },
+  },
+
+  // typescripts
+  // todo: feat/strict-type-check
+  // typescript: {
+  //   strict: true,
+  //   typeCheck: true,
+  // },
+
+  // modules
+  modules: [
+    // chore
+    // '@nuxtjs/eslint-module',
+    // styling & ui
+    '@nuxtjs/tailwindcss',
+    'nuxt-headlessui',
+    'nuxt-icon',
+    '@nuxtjs/color-mode',
+    // management
+    '@pinia/nuxt',
+    '@vueuse/nuxt',
+    // contents,
+    '@nuxt/content',
+
+    // todo: feat/localization
+    // '@nuxtjs/i18n'
+  ],
+
+  css: [
+    resolve('./assets/scss/_variables.scss'),
+    resolve('./assets/scss/app.scss'),
+  ],
+
+  components: [
+    {
+      prefix: 'Layout',
+      path: resolve('./components/layouts'),
+      global: true,
+    },
+    {
+      prefix: 'Awesome',
+      path: resolve('./components/awesome'),
+      global: true,
+    },
+  ],
+
+  imports: {
+    dirs: [resolve('./stores'), '~/stores'],
+  },
+
+  // module::pinia
+  pinia: {
+    autoImports: [['defineStore', 'definePiniaStore']],
+  },
+
+  // module::headlessui
+  headlessui: {
+    prefix: 'Headless',
+  },
+
+  // module::color-mode
+  colorMode: {
+    classSuffix: '',
+  },
+
+  // module::content
+  content: {
+    documentDriven: true,
+    markdown: {
+      mdc: true,
+    },
+    highlight: {
+      theme: 'github-dark',
+    },
+  },
+
+  build: {
+    transpile: ['vue-i18n'],
+  },
+  vite: {
+    plugins: [
+      VueI18nVitePlugin({
+        include: [
+          resolve(
+            dirname(fileURLToPath(import.meta.url)),
+            './configs/locales/*.json'
+          ),
+        ],
+      }),
+    ],
+  },
+})
---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+const { awesome } = useAppConfig()
+definePageMeta({ layout: 'page' })
+useHead({ titleTemplate: '', title: awesome?.name || 'Nuxt 3 Awesome Starter' })
+</script>
+
+<template>
+  <div>
+    <h1>{{ $t('hello', { name: 'vue-i18n' }) }}</h1>
+    <form>
+      <label for="locale-select">{{ $t('language') }}: </label>
+      <select id="locale-select" v-model="$i18n.locale">
+        <option value="en">en</option>
+        <option value="vi">vi</option>
+      </select>
+    </form>
+  </div>
+</template>
