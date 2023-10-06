e1ac18f3797dcfdbeb2800e31550d16784f7dae4
Nguyen Trung <nguyentrung@Nguyens-iMac.local>
2023-10-06 14:13:48 +0700

    add Link

--- pages/components.vue
+++ pages/components.vue
@@ -7,5 +7,6 @@ useHead({ titleTemplate: 'Components' })
   <div>
     <CommonIcon name="i-heroicons-light-bulb" color="red" />
     <!-- <CommonAvatar /> -->
+    <CommonLink>Link</CommonLink>
   </div>
 </template>
