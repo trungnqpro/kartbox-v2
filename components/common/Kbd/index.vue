ad8c1eb2e84ee054f8252ce08e307f0cf6ca4e5b
hoanghuy <hoanghuybaw@gmail.com>
2023-10-11 09:29:45 +0700

    Add dropdown common

---
+++ components/common/Kbd/index.vue
@@ -0,0 +1,73 @@
+<template>
+  <kbd :class="kbdClass" v-bind="attrs">
+    <slot>{{ value }}</slot>
+  </kbd>
+</template>
+
+<script lang="ts">
+import { toRef, defineComponent, computed } from 'vue'
+import type { PropType } from 'vue'
+import { twMerge, twJoin } from 'tailwind-merge'
+import { useUI } from '../../../composables/ui/useUI'
+import { mergeConfig } from '../../../utils'
+import type { Strategy } from '../../../types'
+// @ts-expect-error
+import appConfig from '#build/app.config'
+
+const config = mergeConfig<typeof kbd>(
+  appConfig.ui.strategy,
+  appConfig.ui.kbd,
+)
+
+export default defineComponent({
+  inheritAttrs: false,
+  props: {
+    value: {
+      type: String,
+      default: null,
+    },
+    size: {
+      type: String as PropType<keyof typeof config.size>,
+      default: () => config.default.size,
+      validator(value: string) {
+        return Object.keys(config.size).includes(value)
+      },
+    },
+    class: {
+      type: [String, Object, Array] as PropType<any>,
+      default: undefined,
+    },
+    ui: {
+      type: Object as PropType<
+        Partial<typeof config & { strategy?: Strategy }>
+      >,
+      default: undefined,
+    },
+  },
+  setup(props) {
+    const { ui, attrs } = useUI('kbd', toRef(props, 'ui'), config)
+
+    const kbdClass = computed(() => {
+      return twMerge(
+        twJoin(
+          ui.value.base,
+          ui.value.size[props.size],
+          ui.value.padding,
+          ui.value.rounded,
+          ui.value.font,
+          ui.value.background,
+          ui.value.ring
+        ),
+        props.class
+      )
+    })
+
+    return {
+      // eslint-disable-next-line vue/no-dupe-keys
+      ui,
+      attrs,
+      kbdClass,
+    }
+  },
+})
+</script>
